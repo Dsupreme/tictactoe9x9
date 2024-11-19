1921aad693dcd31b25b8d33e3e2dde6a43ccea89
Manmeet S. Oberoi <ms_oberoi@outlook.com>
2024-11-19 17:41:50 +0530

    Updated vite build base path to match github pages path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "tic-tac-toe-advanced",
+  base: "/tictactoe9x9/",
 });
